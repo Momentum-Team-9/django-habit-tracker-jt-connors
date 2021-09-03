d8be11f69128a7f562c37457df9bb5c007f9255d
“Joseph <jtconnors94@gmail.com>
2021-09-02 20:01:22 -0400

    changed models and updated templates

--- core/models.py
+++ core/models.py
@@ -9,8 +9,7 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 class Habit(models.Model):
-    title = models.CharField(max_length=250)
-    goal = models.TextField(blank=True, null=True)
+    goal = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         User,
@@ -22,19 +21,20 @@ class Habit(models.Model):
         return f'{self.title}'
 
 class DailyRecord(models.Model):
+    result = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     habit = models.ForeignKey(
         Habit,
         on_delete=models.CASCADE,
         related_name="records")
-
-    #class Meta:
-     #   constraint = models.UniqueConstraint(
-      #      fields=['habit', 'date'], name='unique_record')
+    class Meta:
+        constraint = [
+            UniqueConstraint(fields=['habit', 'date'], name='unique_record')
+        ]
     
-    #def __repr__(self):
-    #    return f"<Daily Record date={self.date}>"
+        def __repr__(self):
+            return f"<Daily Record date={self.date}>"
 
-    #def __str__(self):
-    #    return f"{self.date}"
+        def __str__(self):
+            return f"{self.date}"
     
